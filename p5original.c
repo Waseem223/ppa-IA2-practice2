19202c3b827f1d8ce4c0c3552a3f8d18f902dd36
Waseem223 <93112692+Waseem223@users.noreply.github.com>
2022-03-23 09:17:58 +0000

    done

--- p5original.c
+++ p5original.c
@@ -1,36 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
 int input()
 {
-  int a;
-  printf("Enter a number\n");
-  scanf("%d",&a);
-  return a;
+    int a;
+    printf("enter the numbers \n");
+    scanf("%d",&a);
+    return a;
 }
-int gcd(int a, int b)
+int gcd(int a,int b)
 {
- 
-   int i, gcd;
-
-   
-
-    for(i=1; i <= a && i <= b; ++i)
+    int i,gcd;
+    for(i=a<b?a:b;i>=1;i--)
     {
-        
         if(a%i==0 && b%i==0)
-            gcd = i;
+        {
+            gcd=i;
+            break;
+        }
     }
     return gcd;
 }
-void output(int a,int b, int gcd)
+void output(int a,int b,int ans)
 {
-  printf("HCF of %d and %d is %d",a,b,gcd);
+    printf("the hcf of %d and %d is %d",a,b,ans);
 }
 int main()
 {
-  int a,b,hcf;
-  a=input();
-  b=input();
-  hcf=gcd(a,b);
-  output(a,b,hcf);
-  return 0;
+    int a,b,ans;
+    a=input();
+    b=input();
+    ans=gcd( a, b);
+    output(a, b,ans);
+    return 0;
 }
